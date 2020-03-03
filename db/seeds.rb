079312e1cdd0b57b1464604fdfa8ea6d30c17a97
Maximilian Vosshenrich <12xx12maxi@gmx.de>
2020-03-03 15:45:38 +0000

    Added cloudinary to users and languages. If you dont upload a picture, a default one is set for you

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 require 'faker'
+require 'open-uri'
+
+puts "CLEANING DB"
+User.destroy_all
+Flashcard.destroy_all
+CardSet.destroy_all
+Language.destroy_all
 
 puts "SEEDING USERS"
 
-20.times do
+5.times do
   user = User.new(username: Faker::Internet.username, email: Faker::Internet.email, password: "123456", password_confirmation: "123456", bio: Faker::Hacker.say_something_smart)
   user.save
 end
@@ -16,12 +23,19 @@ f = File.open("db/sourcedata/proglangs.csv", "r")
 f.each_line do |line|
   fields = line.split("|")
   lang = Language.new(name: fields[0].tr_s('"', '').strip, description: fields[1].tr_s('"', '').strip)
-  lang.save
+  file = URI.open(fields[2].tr_s('"', '').strip)
+  # binding.pry
+  lang.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  begin
+    lang.save!
+  rescue
+    puts "Something wrong with #{fields[0].tr_s('"', '').strip}"
+  end
 end
 
 puts "SEEDING FLASHCARDS AND CARD SETS"
 
-200.times do
+10.times do
   card = Flashcard.new(question: Faker::Lorem.question(word_count: 6), correct_answer: "Correct", answer_1: "Wrong 1", answer_2: "Wrong 2", answer_3: "Wrong 3")
   set = CardSet.new(title: "#{Faker::Hacker.ingverb} #{Faker::Hacker.noun}".capitalize, description: Faker::Hacker.say_something_smart, difficulty: ["Easy","Medium","Hard"].sample)
   offset = rand(Language.count)
