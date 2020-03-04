cdef66612ba763afd3655e54f988e87a32cb8981
Shane Wickramasuriya <shane.geeky@gmail.com>
2020-03-04 14:35:26 +0000

    Building the user answers logic

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,5 +1,10 @@
 class UserAnswersController < ApplicationController
   def create
+    param
+    @user_answer = UserAnswer.new
+    @user_answer.user = current_user
+    @user_answer.flashcard = Flashcard.find(params)
+    redirect_to "/results"
   end
 
   def update
