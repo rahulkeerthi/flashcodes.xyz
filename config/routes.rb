6fff1fd09f5a5fa14cfd38adfea6a0545c03e744
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-03-04 15:42:28 +0000

    cards updated, search styling also completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :user_answers, only: [:create, :update, :index]
   devise_for :users
   root to: 'pages#home'
-
   resources :users, only: :show
+
+# FOR TESTING CSS AND STYLES
+  get 'test', to: 'pages#test'
+
 end
