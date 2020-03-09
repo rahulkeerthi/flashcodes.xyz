f1a0411002519377e6fd6b36c3721d24d206e6c6
Maximilian Vosshenrich <12xx12maxi@gmx.de>
2020-03-09 12:58:06 +0000

    Fixed a tiny issue

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -11,6 +11,7 @@ class UserAnswersController < ApplicationController
     difficulty = flashcard.card_set.difficulty.to_sym
     user_set = UserSet.find(params[:user_answer][:user_set])
     answer = params[:user_answer][:answer]
+    group = GroupMembership.find_by(user: current_user, language: user_set.card_set.language)
     if answer == flashcard.correct_answer
       user_answer = UserAnswer.where(flashcard: flashcard, user_set: user_set).first
       unless user_answer.correct
@@ -22,8 +23,7 @@ class UserAnswersController < ApplicationController
         current_user.points += POINTS[difficulty]
         current_user.save
         # logic to add earned points to group points
-        group = GroupMembership.find_by(user: current_user, language: user_set.card_set.language)
-        group.points += user_set.points_earned
+        group.points += POINTS[difficulty]
         group.save
       end
     end
