14b7affdef490edbd0807f31a7ca28939ef02725
Maximilian Vosshenrich <12xx12maxi@gmx.de>
2020-03-10 15:00:26 +0000

    Changed some dependencies to fix the seed file for GroupMemberschips and that users belong to a group throguh the membership

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   has_many :group_memberships, dependent: :destroy
+  has_many :users, through: :group_memberships
   belongs_to :language
   validates :name, presence: true
 end
