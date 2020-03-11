5b4fb08d9ec658c567cd207bfb54da277a5756dc
Tomas Kudlac <kudlac.tomas@gmail.com>
2020-03-11 12:56:29 +0000

    notification stuff

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,4 +1,4 @@
-class UserAnswersController < ApplicationController
+  class UserAnswersController < ApplicationController
   POINTS = {
       "Easy": 10,
       "Medium": 20,
@@ -24,21 +24,43 @@ class UserAnswersController < ApplicationController
         user_answer.save
         # logic for adding points
         user_set.points_earned += POINTS[difficulty]
+        user_set.save
         current_user.points += POINTS[difficulty]
         current_user.save
         # logic to add earned points to group points
         membership.points += POINTS[difficulty]
         membership.save
         if (group.group_memberships.calculate(:sum, :points)) >= group.target_points
-          group.level += 1
-          group.target_points += BASE_LEVEL_PTS * group.level
-          group.save
+          # limit max level and level incrementation
+          unless group.level >= 4
+            group.level += 1
+            group.target_points += BASE_LEVEL_PTS * group.level
+            group.save
+            group.users.each do |recipient|
+              Notification.create(
+                recipient: recipient,
+                actor: current_user,
+                content: "Your #{group.name} has leveled up! Level: #{group.level}")
+            end
+          end
         end
       end
     end
 
-    user_set.completed = user_set.user_answers.all? { |answer| answer.correct }
-    user_set.save
+    if user_set.user_answers.all? { |answer| answer.correct }
+      user_set.completed = true
+      user_set.save
+      # creating recipients for the notifications, substracting the current user, put into an array so that we can substract from the users array
+      group.users.each do |recipient|
+        unless recipient == current_user
+          Notification.create(
+            recipient: recipient,
+            actor: current_user,
+            action: card_set_path(user_set.card_set),
+            content: "#{current_user.username.capitalize} has completed the #{user_set.card_set.title} set.")
+        end
+      end
+    end
   end
 
 end
--- db/migrate/20200309165322_add_answer_to_user_answers_again.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswerToUserAnswersAgain < ActiveRecord::Migration[6.0]
-  def change
-    add_column :user_answers, :answer, :string
-  end
-end
