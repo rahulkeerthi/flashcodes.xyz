bc65205b425d190150e4e248945ff8822e690913
Maximilian Vosshenrich <12xx12maxi@gmx.de>
2020-03-11 14:29:22 +0000

    Added a level column to the user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_165623) do
+ActiveRecord::Schema.define(version: 2020_03_11_142738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_165623) do
     t.text "bio"
     t.integer "points", default: 0
     t.boolean "admin"
+    t.integer "level", default: 1
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
