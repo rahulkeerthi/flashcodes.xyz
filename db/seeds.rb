0adbd9289ce024744895c3ccd456bf9aff974df0
Maximilian Vosshenrich <12xx12maxi@gmx.de>
2020-03-12 15:08:33 +0000

    Run rails db:reset to have a fully functional and rich DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,6 @@ require 'csv'
 require 'faker'
 require 'open-uri'
 
-puts "CLEANING DB"
-Group.destroy_all
-GroupMembership.destroy_all
-UserSet.destroy_all
-UserAnswer.destroy_all
-User.destroy_all
-CardSet.destroy_all
-Flashcard.destroy_all
-Language.destroy_all
-
 puts "SEEDING USERS"
 
 5.times do
@@ -22,13 +12,6 @@ puts "SEEDING USERS"
   user.photo.attach(io: file, filename: 'nes.png')
   user.save!
 end
-
-puts "Adding ADMIN!"
-
-user = User.new(username: "admin", email: "admin@admin.com", password: "123456", password_confirmation: "123456", bio: Faker::Hacker.say_something_smart, admin: true)
-  file = URI.open("https://loremflickr.com/500/500/person")
-  user.photo.attach(io: file, filename: 'nes.png')
-  user.save!
 
 puts "DONE! SEEDING LANGUAGES"
 
@@ -50,11 +33,11 @@ end
 puts "DONE! SEEDING FLASHCARDS AND CARD SETS"
 
 Language.all.each do |lang|
-  10.times do
+  rand(10..15).times do
     set = CardSet.new(title: "#{Faker::Hacker.ingverb} #{Faker::Hacker.noun}".capitalize, description: Faker::Hacker.say_something_smart, difficulty: ["Easy","Medium","Hard"].sample)
     set.language = lang
     set.save
-    10.times do
+    rand(10..15).times do
       card = Flashcard.new(question: Faker::Lorem.question(word_count: 6), correct_answer: "Correct- #{Faker::Hacker.say_something_smart} #{Faker::Hacker.say_something_smart}", answer_1: "Wrong 1 - #{Faker::Hacker.say_something_smart} #{Faker::Hacker.say_something_smart}", answer_2: "Wrong 2 - #{Faker::Hacker.say_something_smart} #{Faker::Hacker.say_something_smart}", answer_3: "Wrong 3 - #{Faker::Hacker.say_something_smart} #{Faker::Hacker.say_something_smart}")
       card.card_set = set
       card.save
@@ -62,3 +45,33 @@ Language.all.each do |lang|
   end
 end
 
+puts "CREATING GROUPS"
+
+Language.all.each do |lang|
+  group = Group.new(name: "#{Faker::Coffee.blend_name} #{Faker::Creature::Animal.name.capitalize}s", full: false, language:lang)
+  group.save
+end
+
+puts "GROUPS DONE - CREATING MEMBERSHIPS"
+
+User.first(5).each do |user|
+  Group.all.sample(10).each do |group|
+    random_points = (rand(1..10) * 10)
+    GroupMembership.create(user: user, group: group, points: random_points)
+    puts "membership done!"
+  end
+  random_points = (rand(1..10) * 10)
+  user.points = random_points
+  user.save
+  puts "user done!"
+end
+
+
+puts "Adding ADMIN!"
+
+user = User.new(username: "admin", email: "admin@admin.com", password: "123456", password_confirmation: "123456", bio: Faker::Hacker.say_something_smart, admin: true)
+  file = URI.open("https://loremflickr.com/500/500/person")
+  user.photo.attach(io: file, filename: 'nes.png')
+  user.save!
+
+puts "ALL DONE! :)"
